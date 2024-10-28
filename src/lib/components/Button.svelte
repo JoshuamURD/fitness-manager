b98a1b72d8d8fe6e1833eae36d671cb2e4802242
joshuamURD <jmacmaster9@gmail.com>
2024-10-28 22:17:41 +0800

    first commit

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+import type { Snippet } from "svelte";
+
+interface BasicProps {
+    children: Snippet
+}
+
+interface ButtonProps extends BasicProps {
+    type?: "button" | "submit" | "reset";
+    href?: never;
+}
+interface LinkProps extends BasicProps {
+    href: string;
+}
+
+type ComponentProps = ButtonProps | LinkProps;
+
+let {children, href, ...props}: ComponentProps = $props();
+
+</script>
+
+{#if href}
+<a href={href}>
+    {@render children()}
+</a>
+{:else}
+
+<button {...props} class="btn">
+    {@render children()}
+</button>
+{/if}
---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,24 @@
+<script>
+  import heroImage from "$assets/Hero.jpg";
+</script>
+
+<section
+  class="relative h-screen bg-cover bg-center bg-no-repeat"
+  style="background-image: url('{heroImage}')"
+>
+  <div
+    class="container mx-auto px-4 py-16 text-white from-slate-900/50 via-zinc-900 to-zinc-700 bg-gradient-to-br rounded-lg py-20"
+  >
+    <h1 class="text-6xl font-bold font-serif mt-4">
+      Welcome to fitness manager
+    </h1>
+    <p class="text-xl font-thin m-2 font-serif">
+      Your body as a science experiment. You mad scientist.
+    </p>
+    <p class="text-lg my-10 mx-2">
+      Start pulling in information from various data collection points.
+      MyFitnessPal, Hevy and Fitbit come together and then through AI have
+      analysis done on your data to help you set better goals for your fitness.
+    </p>
+  </div>
+</section>
